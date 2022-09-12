f6bac548741a72743a3902d71f42fd6c688c10b6
Edward O'Connor <edwardoc@buffalo.edu>
2022-09-12 00:25:10 -0400

    Added more design to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Nav } from './components/Nav';
+import { Homepage } from './pages/Homepage';
 
 
 
@@ -11,12 +12,17 @@ function App() {
 
 
     return (
-
+        <BrowserRouter>
        
             <header>
                 <Nav />
             </header>
-            
+
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+            </Routes>
+
+        </BrowserRouter> 
 
 
            
---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import react from 'react';
+
+
+import { TbBallAmericanFootball } from "react-icons/tb";
+
+export const Homepage = () => {
+    return (
+        <Contain>
+            <div className="Container">
+
+                <div className="container" id="welcome">
+                    <center>
+                        <div className="container"><h1 className="header">Welcome to Twelve Wise Men</h1></div>
+                    </center>
+
+                    <div className="card">
+                        
+                        <center><h5 className="card-header">Sign Up or Login to use the Trade Post!</h5></center>
+                        <div className="card-body">
+                            <center><a href="#" className="btn btn-primary">Sign Up / Login</a></center>
+                        </div>
+                    </div>
+
+
+                    <div className="card" id="details">
+
+                        <center><h5 className="card-header">Last Seasons Leauge Winner (2021 Season)</h5></center>
+                        <div className="card-body">
+                           <center> <h4>Timmy Jones</h4> </center>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </Contain>
+        
+        );
+}
+
+
+const Contain = styled.section`
+
+#welcome {
+
+    
+    width: 300px;
+    height: 60%;
+    position: absolute;
+    top:20%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    background-color: black;
+    width: 70%;
+
+
+.header {
+font-family: 'Kanit', sans-serif bold;
+color: blue;
+}
+
+}
+
+.card {
+background-color: white;
+}
+
+#details {margin-top:5rem;
+
+h5 {color: white;}
+
+h4 {color: blue;}
+
+
+background-color: black;
+
+}
+ 
+    
+`;
